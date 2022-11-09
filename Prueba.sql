11cd77ace85e8c88672d6ccfdc1550e1bd5bf991
Eduardosscc21 <114125293+Eduardosscc21@users.noreply.github.com>
2022-11-09 12:30:31 -0600

    Add files via upload

---
+++ Prueba.sql
@@ -0,0 +1,253 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: calidadeducativa
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clase`
+--
+
+DROP TABLE IF EXISTS `clase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clase` (
+  `idclase` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idclase`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clase`
+--
+
+LOCK TABLES `clase` WRITE;
+/*!40000 ALTER TABLE `clase` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `escuela`
+--
+
+DROP TABLE IF EXISTS `escuela`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `escuela` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `ubicacion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `escuela`
+--
+
+LOCK TABLES `escuela` WRITE;
+/*!40000 ALTER TABLE `escuela` DISABLE KEYS */;
+INSERT INTO `escuela` VALUES (1,'Colegio insurgentes','Pedro buzeta'),(2,'Colegio Jovior','Reforma'),(3,'Colegio Albert Einstein','Rio Blanco'),(4,'Colegio Anahuac','Garibaldi');
+/*!40000 ALTER TABLE `escuela` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiante`
+--
+
+DROP TABLE IF EXISTS `estudiante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiante` (
+  `idEstudiante` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `Grado` varchar(45) NOT NULL,
+  `edad` int DEFAULT NULL,
+  `idmateria` int DEFAULT NULL,
+  PRIMARY KEY (`idEstudiante`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiante`
+--
+
+LOCK TABLES `estudiante` WRITE;
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+INSERT INTO `estudiante` VALUES (1,'Jesus Valentin','4',20,1),(2,'Leon Lopez','5',20,2),(3,'Sergio Perez','5',21,3),(4,'Sarahi Rodriguez','3',19,6),(6,'Sofia Alvarez','3',18,7);
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `horario`
+--
+
+DROP TABLE IF EXISTS `horario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `horario` (
+  `idhorario` int NOT NULL AUTO_INCREMENT,
+  `dia` varchar(45) NOT NULL,
+  `hora_inicio` varchar(45) NOT NULL,
+  `hora_fin` varchar(45) NOT NULL,
+  PRIMARY KEY (`idhorario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `horario`
+--
+
+LOCK TABLES `horario` WRITE;
+/*!40000 ALTER TABLE `horario` DISABLE KEYS */;
+INSERT INTO `horario` VALUES (1,'Lunes','09:00 AM','11:00 AM'),(2,'Miercoles','13:00 PM','15:00 PM'),(3,'Sabado','08:00 AM','14:00 PM'),(4,'Jueves','11:00 AM','13:00 PM'),(5,'Miercoles','07:00 AM','09:00 AM');
+/*!40000 ALTER TABLE `horario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maestro`
+--
+
+DROP TABLE IF EXISTS `maestro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maestro` (
+  `idmaestro` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `edad` int DEFAULT NULL,
+  PRIMARY KEY (`idmaestro`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maestro`
+--
+
+LOCK TABLES `maestro` WRITE;
+/*!40000 ALTER TABLE `maestro` DISABLE KEYS */;
+INSERT INTO `maestro` VALUES (1,'Cesar Perez',18),(2,'Ricardo Sanchez',38),(3,'Sarah Azpeitia',30),(4,'Naim Portillo',24),(5,'Santana Lopez',37);
+/*!40000 ALTER TABLE `maestro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materia`
+--
+
+DROP TABLE IF EXISTS `materia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materia` (
+  `idmateria` int NOT NULL AUTO_INCREMENT,
+  `nombremat` varchar(45) NOT NULL,
+  PRIMARY KEY (`idmateria`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materia`
+--
+
+LOCK TABLES `materia` WRITE;
+/*!40000 ALTER TABLE `materia` DISABLE KEYS */;
+INSERT INTO `materia` VALUES (1,'Ciencias'),(2,'Historia'),(3,'Geografia'),(4,'Economia'),(5,'Arte');
+/*!40000 ALTER TABLE `materia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `periodo`
+--
+
+DROP TABLE IF EXISTS `periodo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `periodo` (
+  `idperiodo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `fecha_inicio` varchar(45) NOT NULL,
+  `fecha_fin` varchar(45) NOT NULL,
+  PRIMARY KEY (`idperiodo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `periodo`
+--
+
+LOCK TABLES `periodo` WRITE;
+/*!40000 ALTER TABLE `periodo` DISABLE KEYS */;
+INSERT INTO `periodo` VALUES (1,'2021A','09/01/2021','15/06/2021'),(2,'2020B','12/08/2020','22/12/2020');
+/*!40000 ALTER TABLE `periodo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `idPersonal` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `cargo` varchar(45) NOT NULL,
+  `edad` int DEFAULT NULL,
+  PRIMARY KEY (`idPersonal`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (1,'Max Avila','Cocinero',24),(2,'Armando Curiel','Intendente',37),(3,'Andres Correa','Lavaloza',22),(4,'Marco Rodriguez','Intendente',26),(5,'Karla Estrada','Cocinera',31);
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problema`
+--
+
+DROP TABLE IF EXISTS `problema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `problema` (
+  `idproblema` int NOT NULL AUTO_INCREMENT,
+  `causas` varchar(150) NOT NULL,
+  `solucion` varchar(150) NOT NULL,
+  PRIMARY KEY (`idproblema`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problema`
+--
+
+LOCK TABLES `problema` WRITE;
+/*!40000 ALTER TABLE `problema` DISABLE KEYS */;
+INSERT INTO `problema` VALUES (1,'Desinteres de los maestros','Motivacion a los profesores'),(2,'Falta de apoyo del gobierno','Solicitar al gobierno apoyo'),(3,'Desinteres del alumnado','Motivar a los alumnos');
+/*!40000 ALTER TABLE `problema` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-08 22:40:41
